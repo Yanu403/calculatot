463c02d4fee49b0ac42c9ef87f71627f37269ee6
Yanu403 <66066867+Yanu403@users.noreply.github.com>
2024-10-03 10:13:34 +0700

    Create Penggunaan.py

---
+++ Penggunaan.py
@@ -0,0 +1,17 @@
+from calculatot import calculator
+
+# Penjumlahan
+hasil = calculator.add(5, 3)
+print(f"Hasil Penjumlahan: {hasil}")
+
+# Pengurangan
+hasil = calculator.subtract(5, 3)
+print(f"Hasil Pengurangan: {hasil}")
+
+# Perkalian
+hasil = calculator.multiply(5, 3)
+print(f"Hasil Perkalian: {hasil}")
+
+# Pembagian
+hasil = calculator.divide(5, 3)
+print(f"Hasil Pembagian: {hasil}")
